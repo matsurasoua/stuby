3d5618dc234c77bb5cfca42c97404354cd48bd0b
matsurasoua <2210344@ecc.ac.jp>
2023-06-27 11:56:30 +0900

    main.dartとassets追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,135 @@
+//appbarとnavigationbar
 import 'package:flutter/material.dart';
+import 'home.dart';
+import 'match.dart';
+import 'chat.dart';
+import 'com.dart';
+import 'profile.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Stuby-勉強マッチング-',
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  // 選択中フッターメニューのインデックスを一時保存する用変数
+  int selectedIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // 切り替える画面のリスト
+  List<Widget> display = [
+    HomePage(),
+    MatchPage(),
+    ChatPage(),
+    ComPage(),
+    ProfilePage()
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      // ヘッダーメニュー
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          title: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top:5), // 上部に8のマージンを追加
+                  child: Image.asset(
+                    'assets/logo.png',
+                    width: 80,
+                    height: 80,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: display[selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                backgroundColor: Colors.white,
+                icon: Icon(
+                  Icons.home_rounded,
+                  color: selectedIndex == 0
+                  ? Colors.lightBlueAccent
+                  : Colors.grey,
+                  size: 30,
+                ),
+                label: 'ホーム', // ラベルのテキストスタイルを指定
+              ),
+              BottomNavigationBarItem(
+                  backgroundColor: Colors.white,
+                  icon: Icon(
+                    Icons.find_in_page_outlined,
+                    color: selectedIndex == 1
+                        ? Colors.lightBlueAccent
+                        : Colors.grey,
+                    size: 30,
+                  ),
+                  label: 'マッチング'),
+              BottomNavigationBarItem(
+                  backgroundColor: Colors.white,
+                  icon: Icon(
+                    Icons.message_outlined,
+                    color: selectedIndex == 2
+                        ? Colors.lightBlueAccent
+                        : Colors.grey,
+                    size: 30,
+                  ),
+                  label: 'チャット'),
+              BottomNavigationBarItem(
+                  backgroundColor: Colors.white,
+                  icon: Icon(
+                    Icons.share_rounded,
+                    color: selectedIndex == 3
+                        ? Colors.lightBlueAccent
+                        : Colors.grey,
+                    size: 30,
+                  ),
+                  label: 'コミュニティ'),
+              BottomNavigationBarItem(
+                  backgroundColor: Colors.white,
+                  icon: Icon(
+                    Icons.person,
+                    color: selectedIndex == 4
+                        ? Colors.lightBlueAccent
+                        : Colors.grey,
+                    size: 30,
+                  ),
+                  label: 'プロフィール'),
+            ],
+            // 現在選択されているフッターメニューのインデックス
+            currentIndex: selectedIndex,
+            // フッター領域の影
+            elevation: 0,
+            // フッターメニュータップ時の処理
+            onTap: (int index) {
+              selectedIndex = index;
+              setState(() {});
+            },
+            // 選択中フッターメニューの色
+            fixedColor: Colors.grey
+        ));
   }
 }
