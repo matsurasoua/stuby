46a51daebb21105e41d3ebc6c544558a2c20059e
杉本童麻 <2210108@ecc.ac.jp>
2023-06-27 12:09:11 +0900

    titile.dartの追加

---
+++ lib/title.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:stuby/RegistrationPage.dart';
+import 'package:stuby/login.dart';
+import 'package:stuby/ProRegPage.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => RegistrationPage(),
+        '/ProReg': (context) => ProRegPage(),
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/stuby_BG.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 250,
+                  height: 350,
+                  child: Image.asset('assets/title.png'),
+                ),
+                OutlinedButton(
+                  child: const Text('新規登録'),
+                  style: OutlinedButton.styleFrom(
+                    primary: Colors.white,
+                    shape: const StadiumBorder(),
+                    side: const BorderSide(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/signup');
+                  },
+                ),
+    // Flutter1.22以降のみ
+                TextButton(
+                  child: const Text('ログイン'),
+                  style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  ),
+                  onPressed: () {
+                   Navigator.pushNamed(context, '/login');
+                  },
+                ),
+                TextButton(
+                  child: const Text('新規登録ページ(Debug用)'),
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/ProReg');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
